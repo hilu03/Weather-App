0aedea39bb2bd6d9c6648be660e9802cca05e6dd
Luong Tran <tranthihienluong2003@gmail.com>
2024-09-10 23:50:54 +0700

    Display information on webpage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,44 @@ import "./styles.css";
 
 const searchButton = document.querySelector(".search-button");
 const inputLocation = document.querySelector(".location");
-const img = document.querySelector("img");
-searchButton.addEventListener("click", () => {
+const result = document.querySelector(".result");
+
+searchButton.addEventListener("click", async () => {
   const location = inputLocation.value;
-  getData(location).then(processJson).then(console.log).catch(console.log);
-  
+  if (location.trim().length === 0) {
+    alert("Please input location first!!!");
+  }
+  else {    
+    try {
+      const json = await getData(location);
+      const data = await processJson(json);
+      console.log(data);
+      displayResult(data);
+    }
+    catch(e) {
+      displayError();
+    }
+  }
 });
+
+function displayResult(data) {
+  const html = 
+  `
+    <div class="location">${data["location"]}</div>
+    <div class="temperature">${data["temp"]}</div>
+    <div class="humidity">${data["humidity"]}%</div>
+    <div class="feel">${data["feel"]}</div>
+    <div class="uv">${data["uv"]}</div>
+    <div class="wind">${data["windspeed"]}</div>
+    <div class="condition">${data["conditions"]}</div>
+  `;
+  result.innerHTML = html;
+}
+
+function displayError() {
+  const html = "-_- Location not found! :(";
+  result.innerHTML = html;
+}
 
 async function getData(location) {
   let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE`, {mode: 'cors'});
@@ -20,13 +52,12 @@ async function getData(location) {
 
 async function processJson(json) {
   const data = {};
-  data["address"] = json["resolvedAddress"];
-  data["timezone"] = json["tzoffset"];
+  data["location"] = json["resolvedAddress"];
   data["temp"] = json["currentConditions"]["temp"];
   data["feel"] = json["currentConditions"]["feelslike"];
   data["humidity"] = json["currentConditions"]["humidity"];
   data["uv"] = json["currentConditions"]["uvindex"];
-  data["icon"] = json["currentConditions"]["icon"];
+  data["windspeed"] = json["currentConditions"]["windspeed"];
   data["conditions"] = json["currentConditions"]["conditions"];
   return data;
 }
