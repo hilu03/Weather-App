90a7aa64920414675a0a9f7b86c94d663fc3e24e
Luong Tran <tranthihienluong2003@gmail.com>
2024-09-10 21:44:14 +0700

    Process json data gotten from API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import "./styles.css";
 
-getData("Đà Nẵng");
+
+const data = getData("Đà Nẵng").then(processJson).then(console.log);
 
 async function getData(location) {
   try {
     let respone = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE`, {mode: 'cors'});
     const json = await respone.json();
     console.log(json);
+    return json;
   }
   catch (e) {
-    console.log("Error!");
+    console.log("Error: " + e);
   }
+}
+
+async function processJson(json) {
+  const data = {};
+  data["address"] = json["resolvedAddress"];
+  data["temp"] = json["currentConditions"]["temp"];
+  data["feel"] = json["currentConditions"]["feelslike"];
+  data["humidity"] = json["currentConditions"]["humidity"];
+  data["uv"] = json["currentConditions"]["uvindex"];
+  data["icon"] = json["currentConditions"]["icon"];
+  return data;
 }
