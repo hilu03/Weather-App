dc7a6903fc45860f7fc4ba5e8b76f781239d7e52
Luong Tran <tranthihienluong2003@gmail.com>
2024-09-10 22:22:47 +0700

    Log data to console  using input

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 import "./styles.css";
 
 
-const data = getData("Đà Nẵng").then(processJson).then(console.log);
+const searchButton = document.querySelector(".search-button");
+const inputLocation = document.querySelector(".location");
+const img = document.querySelector("img");
+searchButton.addEventListener("click", () => {
+  const location = inputLocation.value;
+  getData(location).then(processJson).then(console.log).catch(console.log);
+  
+});
 
 async function getData(location) {
-  try {
-    let respone = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE`, {mode: 'cors'});
-    const json = await respone.json();
-    console.log(json);
-    return json;
+  let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE`, {mode: 'cors'});
+  if (response.status !== 200) {
+    throw new Error("HTTP status " + response.status);
   }
-  catch (e) {
-    console.log("Error: " + e);
-  }
+  return response.json();
 }
 
 async function processJson(json) {
   const data = {};
   data["address"] = json["resolvedAddress"];
+  data["timezone"] = json["tzoffset"];
   data["temp"] = json["currentConditions"]["temp"];
   data["feel"] = json["currentConditions"]["feelslike"];
   data["humidity"] = json["currentConditions"]["humidity"];
   data["uv"] = json["currentConditions"]["uvindex"];
   data["icon"] = json["currentConditions"]["icon"];
+  data["conditions"] = json["currentConditions"]["conditions"];
   return data;
 }
