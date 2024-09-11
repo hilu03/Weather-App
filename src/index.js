44050fa00f52833ac538442aca8366fc11fb3e2e
Luong Tran <tranthihienluong2003@gmail.com>
2024-09-11 17:35:53 +0700

    Add style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./styles.css";
+import temperatureIcon from "./icons/temp.png";
+
+const icons = importAllIcons(require.context('./icons', false, /.svg$/));
 
 
 const searchButton = document.querySelector(".search-button");
-const inputLocation = document.querySelector(".location");
+const inputLocation = document.querySelector(".location-input");
+const degreeSelect = document.querySelector("#degree");
 const result = document.querySelector(".result");
 
 searchButton.addEventListener("click", async () => {
@@ -14,36 +18,75 @@ searchButton.addEventListener("click", async () => {
     try {
       const json = await getData(location);
       const data = await processJson(json);
-      console.log(data);
-      displayResult(data);
+      document.querySelector("body").style.background = `url(${await getGif(data["icon"])}) no-repeat fixed center center / cover`;
+      const degreeUnit = degreeSelect.value;
+      displayResult(data, degreeUnit);
     }
     catch(e) {
-      displayError();
+      displayError(e);
     }
   }
 });
 
-function displayResult(data) {
+async function getGif(condition) {
+  let response = await fetch(`//api.giphy.com/v1/gifs/translate?api_key=f0S3OQshNUs0ZdWgaQaYiBIqqwEuYK6D&s=${condition}`, {mode: 'cors'});
+  if (response.status !== 200) {
+    throw new Error("HTTP status " + response.status);
+  }
+  const json = await response.json();
+  return json.data.images.original.url;
+}
+
+function importAllIcons(r) {
+  const img = {}
+  r.keys().forEach(key => {
+    img[`${key.substring(2, key.indexOf(".svg"))}`] = r(key);
+  });
+  return img;
+}
+
+function CelsiusToFahrenheit(cTemp) {
+  return ((Number(cTemp) * 9 / 5) + 32).toFixed(1);
+}
+
+function displayResult(data, degreeUnit) {
+  if (degreeUnit === "F") {
+    data["temp"] = CelsiusToFahrenheit(data["temp"]);
+    data["feel"] = CelsiusToFahrenheit(data["feel"]);
+  }
   const html = 
   `
-    <div class="location">${data["location"]}</div>
-    <div class="temperature">${data["temp"]}</div>
-    <div class="humidity">${data["humidity"]}%</div>
-    <div class="feel">${data["feel"]}</div>
-    <div class="uv">${data["uv"]}</div>
-    <div class="wind">${data["windspeed"]}</div>
-    <div class="condition">${data["conditions"]}</div>
+    <div class="left">
+      <div class="location">${data["location"]}</div>
+      <div class="condition">${data["conditions"]}</div>
+      <div class="humidity">Humidity: ${data["humidity"]}%</div>
+      <div class="wind">Wind speed: ${data["windspeed"]}km/h</div>
+    </div>
+    <div class="right">
+      <img class="icon" src="${icons[data['icon']]}">
+      <div class="temperature">
+        <div class=temp-icon>
+          <img src=${temperatureIcon}>
+        </div>
+        <div class="temp">
+          ${data["temp"]}&deg;${degreeUnit}
+        </div>
+      </div>
+      <div class="feel">Feel: ${data["feel"]}&deg;${degreeUnit}</div>
+      <div class="uv">UV: ${data["uv"]}</div>
+    </div>
   `;
   result.innerHTML = html;
 }
 
-function displayError() {
+function displayError(e) {
   const html = "-_- Location not found! :(";
   result.innerHTML = html;
+  console.log(e);
 }
 
 async function getData(location) {
-  let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE`, {mode: 'cors'});
+  let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AK7CDA3UGA9PB59F9RYJHRDTE&unitGroup=metric`, {mode: 'cors'});
   if (response.status !== 200) {
     throw new Error("HTTP status " + response.status);
   }
@@ -59,5 +102,6 @@ async function processJson(json) {
   data["uv"] = json["currentConditions"]["uvindex"];
   data["windspeed"] = json["currentConditions"]["windspeed"];
   data["conditions"] = json["currentConditions"]["conditions"];
+  data["icon"] = json["currentConditions"]["icon"];
   return data;
 }
